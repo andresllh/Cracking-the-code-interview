bd8036e452032e12e96a86cd7ff757563bd2993f
Andres <allausas@miners.utep.edu>
2018-05-21 11:38:40 -0600

    4/5 done for CH 3

--- Chapter 3.py
+++ Chapter 3.py
@@ -34,29 +34,183 @@ def stack (stack_object):
 	def Min(self):
 		return stack_object.min_value
 
-# Problem
+# Problem 3.4 Queue via Stacks
 """
-I: 
-D: 
-E/A: 
-L: 
+I: Implement a queue using two stacks.
+D: Find a way to implement a queue using two stacks. 
+E/A: Used Duke's 7 steps. This problem was discussed in class. During the class discussion I had come up with a solution that
+	 ran in O(n) time complexity, but another solution was shown that had the same time complexity, but only in specific cases. 
+	 The idea was to create two stacks, one where everything will be pushed and the second one, where everything will be popped.
+	 Since the stacks only have one purpose to them, as long as the dequeue stack is not empty the queue will dequeu in O(1) time 
+	 complexity. The case where you don't achieve constant time operations is when the dequeue stack is empty, so everything from
+	 the enqueue stack needs to be popped and pushed into the dequeue stack. 
+L: From the idea that was given in class I was able to design a queue that uses two stacks to implement it's functionality. Using
+   the idea given in class, the time complexity for most operations would be O(1), unless you have an empty dequeue stack, which will
+   be O(n) running time, where n is the size of the enqueue stack. 
 """
+def queue():
+	stack1 = stack()
+	stack2 = stack()
+	last_stack_one = True
 
-# Problem
-"""
-I: 
-D: 
-E/A: 
-L: 
-"""
+	def enqueue(data):
+		stack2.push()
 
-# Problem
+	def dequeue():
+		if self.isEmptyQueue:
+			throw exception
+		if stack1.isEmpty():
+			while not(stack2.isEmpty):
+				stack1.push(stack2.pop())
+		else:
+			stack1.pop()
+		return
+
+	def peekQueue():
+		if self.isEmptyQueue:
+			throw exception
+		if stack1.isEmpty():
+			while not(stack2.isEmpty):
+				stack1.push(stack2.pop())
+		else:
+			return stack1.peek()
+
+	def isEmptyQueue():
+		return stack1.isEmpty() and stack2.isEmpty()
+
+# Problem 3.5 Sort Stack
 """
-I: 
-D: 
-E/A: 
-L: 
+I: Sort a stack with the smallest items on top.
+D: Find a way to sort a stack with the smallest items ontop while using no additional data structures, except a temporary stack.
+E/A: For this problem, I first get the size of the stack, which also returns the first largest item in the stack. I then created a loop that
+	 iterates until the whole stack is ordered. By using the size of the stack, every time I find the next largest item in the stack, I decrease
+	 the size variable by 1. The size variable stops the popping of the stack right before the last ordered item was popped and iterates through
+	 the remaining elements in the stack until it finds the next largest element and pushing it back into the stack and once again decreasing
+	 the size by 1, until the size is 1, which means the smallest item is at the top, then it terminates the loop. 
+L: For this problem I learned how to use a temporary stack in combination with some other variables to order the stack from the largest
+   to smallest element. 
 """
+def sort_stack(stack1):
+	def get_size(stack1):
+		size = 0
+		largest_item = None
+		while not(stack1.isEmpty):
+			curr = stack1.pop()
+			if curr > largest_item or largest_item is None:
+				largest_item = curr
+			size += 1
+		return size, largest_item
+
+	if stack1.isEmpty():
+		return
+
+	temp_stack = stack()
+	temp_stack.push(largest_item)
+	size, largest_item = get_size(stack1)
+	index = 0
+	while True:
+		if size == 1:
+			break
+		curr = stack1.pop()
+		if index < size:
+			if curr > largest_item or largest_item is None:
+				largest_item = curr
+			temp_stack.push(curr)
+			index += 1
+
+		stack1.push(largest_item)
+		index = 0
+		largest_item = None
+		size -= 1
+
+# Problem 3.6 Animal Shelter
+"""
+I: Design a queue where the eldest animal is adopted.
+D: Design a queue where the eldest animal is adopted, but they may select their preference of dog or cat, else they get the eldest animal. 
+E/A: Used Duke's 7 steps. For this problem I had to freshen up on my queue structure by reading over the sample code that was given to us in the book.
+	 After reading up on queues I created a modified queue for the animal shelter that contains two different queues, one for cats and one for dogs.
+	 For enqueueing, you just need to check the animal type, which is an attribute from the animal object that I created. If the animal is a dog, it 
+	 will be enqueued in the dog queue, else it will be enqueued into the cat queue. For dequeueing, 3 different methods were created, one to dequeue the
+	 oldest animal, and 2 more methods to dequeue a dog or cat. 
+L: For this problem I had to relearn how to create a queue by utilizing the book as a resource. From the sample code that was given, I was able to implement
+   my solution to the problem.
+"""
+public void animal_shelter() {
+	void Animal(String name, String type, int time) {
+		this.name = name;
+		this.type = type;
+	}
+
+	Node first_dog;
+	Node last_dog;
+	Node first_cat;
+	Node last_cat;
+
+	void enqueue(Animal animal) {
+		Node temp = new Node(animal);
+		if (animal.type.equals('dog')) {
+			if (last_dog != null) {
+				last_dog.next = temp;
+				
+			} 
+			last_dog = temp;
+			if (first_dog == null) {
+				first_dog = last_dog;
+			}
+		} else {
+			if (last_cat != null) {
+				last_cat.next = temp;
+			}
+			last_cat = temp;
+			if (first_cat == null) {
+				first_cat = last_cat;
+			}
+		}
+	}
+
+	void dequeue_cat() {
+		if (first_cat == null) {
+			throw exception
+			return;
+		} 
+		Node temp = first_cat.data;
+		first_cat = first_cat.next;
+		if (first_cat == null) {
+			last_cat = null;
+		}
+	}
+
+	void dequeue_dog() {
+		if (first_dog == null) {
+			throw exception
+			return;
+		}
+		Node temp = first_dog.data;
+		first_dog = first_dog.next;
+		if (first_dog == null) {
+			last_dog = null;
+		}
+	}
+
+	void dequeue_any() {
+		if (first_dog == null && first_cat == null) {
+			throw exception
+			return;
+		}
+		if (first_dog == null) {
+			dequeue_cat();
+		}
+		if (first_cat == null) {
+			dequeue_dog();
+		}
+		if (first_cat.data.time > first_dog.data.time) {
+			dequeue_cat();
+		} else {
+			dequeue_dog()
+		}
+	}
+
+}
 
 # Problem
 """
