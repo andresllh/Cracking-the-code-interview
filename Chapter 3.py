304d8d7dd8ab7b249cc9c5610dbc7fde4c729f29
Andres <allausas@miners.utep.edu>
2018-05-21 00:54:05 -0600

    1/5 problems complete for CH 3

---
+++ Chapter 3.py
@@ -0,0 +1,67 @@
+# Problem 3.2 Stack Min
+"""
+I: Create a stack that has Min function that works in O(1) as well as push and pop in O(1)
+D: Find a way to create a stack that can perform min, push, and pop in O(1) time complexity
+E/A: Used Duke's 7 steps. This problem was discussed during class, so I had a similar approach to the problem.
+	 While the syntax might not be correct for creating objects and its functions, the ideas are still there
+	 this problem is written in something that resembles pseudo code. My approach for this problem was to create
+	 a stack object that will be in the stack. This stack object will store the min value of the stack plus it's own data.
+	 By storing the min value in the object, you are able to obtain O(1) time complexity for all 3 functions.
+L: For this problem I utilized the structure that was given to us by the Professor. Since a stack can store any object you
+   want to store in it. You are able to store any type of data when you push and have that be available in a O(1) response time. 
+"""
+def stack (stack_object):
+	self.stack_object
+	self.min_value
+
+	def stack_object(self, data):
+		self.data = data
+		self.min_value = stack.min_value
+
+	def push(self, stack_object):
+		if stack_object.min_value < stack.min_value:
+			stack.min_value = stack_object.min_value
+
+	def pop(self):
+		return stack.stack_object
+
+	def peek(self):
+		return stack_object.data, stack_object.min_value
+
+	def isEmpty(self):
+		return stack == None
+
+	def Min(self):
+		return stack_object.min_value
+
+# Problem
+"""
+I: 
+D: 
+E/A: 
+L: 
+"""
+
+# Problem
+"""
+I: 
+D: 
+E/A: 
+L: 
+"""
+
+# Problem
+"""
+I: 
+D: 
+E/A: 
+L: 
+"""
+
+# Problem
+"""
+I: 
+D: 
+E/A: 
+L: 
+"""
